e1ac247ebe3f5a86aaf2f2488868b37b99a39ff2
sangib58 <sangibruse@gmail.com>
2025-02-08 22:30:24 +0600

    logo changed

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -3,111 +3,108 @@
         <template v-slot:prepend>
             <v-app-bar-nav-icon>
                 <v-avatar>
-                    <v-img
-                        alt="logo"
-                        :src="logoSrc"
-                    ></v-img>
+                    <v-img alt="logo" :src="logoSrc"></v-img>
                 </v-avatar>
             </v-app-bar-nav-icon>
-        </template>      
+        </template>
         <template v-slot:append>
             <div class="mr-8" v-if="$vuetify.display.mdAndUp">
-                <v-btn variant="text" class="text-capitalize" :href="this.aboutUrl"><span class="numberStyle">01.</span> <span class="textStyle">About</span></v-btn>
-                <v-btn variant="text" class="text-capitalize" :href="this.experienceUrl"><span class="numberStyle">02.</span> <span class="textStyle">Experience</span></v-btn>
-                <v-btn variant="text" class="text-capitalize" :href="this.workUrl"><span class="numberStyle">03.</span> <span class="textStyle">Work</span></v-btn>
-                <v-btn variant="text" class="text-capitalize" :href="this.contactUrl"><span class="numberStyle">04.</span> <span class="textStyle">Contact</span></v-btn>
-                <v-btn variant="outlined" class="text-capitalize" :href="this.resumeUrl" color="#53D3B9" target="_blank">Resume</v-btn>
-            </div>  
+                <v-btn variant="text" class="text-capitalize" :href="this.aboutUrl"><span class="numberStyle">01.</span>
+                    <span class="textStyle">About</span></v-btn>
+                <v-btn variant="text" class="text-capitalize" :href="this.experienceUrl"><span
+                        class="numberStyle">02.</span> <span class="textStyle">Experience</span></v-btn>
+                <v-btn variant="text" class="text-capitalize" :href="this.workUrl"><span class="numberStyle">03.</span>
+                    <span class="textStyle">Work</span></v-btn>
+                <v-btn variant="text" class="text-capitalize" :href="this.contactUrl"><span
+                        class="numberStyle">04.</span> <span class="textStyle">Contact</span></v-btn>
+                <v-btn variant="outlined" class="text-capitalize" :href="this.resumeUrl" color="#53D3B9"
+                    target="_blank">Resume</v-btn>
+            </div>
             <div v-else>
                 <v-app-bar-nav-icon color="#64FFDA" variant="text" @click.stop="drawer = !drawer">
                 </v-app-bar-nav-icon>
-            </div>        
-        </template>       
+            </div>
+        </template>
     </v-app-bar>
-    <v-navigation-drawer
-        v-model="drawer"
-        :location="$vuetify.display.mobile ? 'bottom' : undefined"
-        temporary
-      >
-      <div class="d-flex flex-column justify-center">
-        <div class="d-flex flex-column text-center my-4">
-            <div class="numberStyle my-0 py-0">01.</div>
-            <v-btn variant="text" class="text-capitalize my-0 py-0" :href="this.aboutUrl"><span class="textStyle">About</span></v-btn>
-        </div> 
-        <div class="d-flex flex-column text-center my-4">
-            <div class="numberStyle my-0 py-0">02.</div>
-            <v-btn variant="text" class="text-capitalize my-0 py-0" :href="this.experienceUrl"><span class="textStyle">Experience</span></v-btn>
-        </div>
-        <div class="d-flex flex-column text-center my-4">
-            <div class="numberStyle my-0 py-0">03.</div>
-            <v-btn variant="text" class="text-capitalize my-0 py-0" :href="this.workUrl"><span class="textStyle">Work</span></v-btn>
-        </div>  
-        <div class="d-flex flex-column text-center my-4">
-            <div class="numberStyle my-0 py-0">04.</div>
-            <v-btn variant="text" class="text-capitalize my-0 py-0" :href="this.contactUrl"><span class="textStyle">Contact</span></v-btn>
-        </div>
-        <div class="my-4">
+    <v-navigation-drawer v-model="drawer" :location="$vuetify.display.mobile ? 'bottom' : undefined" temporary>
+        <div class="d-flex flex-column justify-center">
+            <div class="d-flex flex-column text-center my-4">
+                <div class="numberStyle my-0 py-0">01.</div>
+                <v-btn variant="text" class="text-capitalize my-0 py-0" :href="this.aboutUrl"><span
+                        class="textStyle">About</span></v-btn>
+            </div>
+            <div class="d-flex flex-column text-center my-4">
+                <div class="numberStyle my-0 py-0">02.</div>
+                <v-btn variant="text" class="text-capitalize my-0 py-0" :href="this.experienceUrl"><span
+                        class="textStyle">Experience</span></v-btn>
+            </div>
+            <div class="d-flex flex-column text-center my-4">
+                <div class="numberStyle my-0 py-0">03.</div>
+                <v-btn variant="text" class="text-capitalize my-0 py-0" :href="this.workUrl"><span
+                        class="textStyle">Work</span></v-btn>
+            </div>
+            <div class="d-flex flex-column text-center my-4">
+                <div class="numberStyle my-0 py-0">04.</div>
+                <v-btn variant="text" class="text-capitalize my-0 py-0" :href="this.contactUrl"><span
+                        class="textStyle">Contact</span></v-btn>
+            </div>
+            <div class="my-4">
+                <div class="d-flex justify-center">
+                    <v-btn variant="outlined" class="text-capitalize" :href="this.resumeUrl" color="#53D3B9"
+                        target="_blank">Resume</v-btn>
+                </div>
+            </div>
             <div class="d-flex justify-center">
-                <v-btn variant="outlined" class="text-capitalize" :href="this.resumeUrl" color="#53D3B9" target="_blank">Resume</v-btn>
-            </div>      
+                <v-btn v-for="icon in icons" :key="icon.route" :icon="icon.img" :href="icon.route" size="x-large"
+                    variant="text" target="_blank">
+                </v-btn>
+            </div>
         </div>
-        <div class="d-flex justify-center">
-            <v-btn
-            v-for="icon in icons"
-            :key="icon.route"
-            :icon="icon.img"
-            :href="icon.route"
-            size="x-large"
-            variant="text"
-            target="_blank"
-            >
-            </v-btn>
-        </div>         
-      </div>
-        
+
     </v-navigation-drawer>
 </template>
 
 <script>
 export default {
-    name:'App Bar',
-    data(){
-        return{
+    name: 'App Bar',
+    data() {
+        return {
             drawer: false,
             group: null,
             icons: [
-                {img:'mdi-linkedin',route:'https://www.linkedin.com/in/sangib-kumar-saha-663914a1/',variant:'elevated'},
-                {img:'mdi-github',route:'https://github.com/sangib58',variant:'elevated'},
-                {img:'mdi-facebook',route:'https://www.facebook.com/shuvo58',variant:'elevated'},
-                {img:'mdi-instagram',route:'https://www.instagram.com/shuvo58/',variant:'elevated'}   
+                { img: 'mdi-linkedin', route: 'https://www.linkedin.com/in/sangib-kumar-saha-663914a1/', variant: 'elevated' },
+                { img: 'mdi-github', route: 'https://github.com/sangib58', variant: 'elevated' },
+                { img: 'mdi-facebook', route: 'https://www.facebook.com/shuvo58', variant: 'elevated' },
+                { img: 'mdi-instagram', route: 'https://www.instagram.com/shuvo58/', variant: 'elevated' }
             ],
-            aboutUrl:window.location.origin+'/#about',
-            workUrl:window.location.origin+'/#work',
-            experienceUrl:window.location.origin+'/#experience',
-            contactUrl:window.location.origin+'/#contact',
-            resumeUrl:window.location.origin+'/img/Sangib-Resume.pdf'
+            aboutUrl: window.location.origin + '/#about',
+            workUrl: window.location.origin + '/#work',
+            experienceUrl: window.location.origin + '/#experience',
+            contactUrl: window.location.origin + '/#contact',
+            resumeUrl: window.location.origin + '/img/Sangib-Resume.pdf'
         }
     },
     watch: {
-      group () {
-        this.drawer = false
-      },
+        group() {
+            this.drawer = false
+        },
     },
-    computed:{
-        logoSrc(){
-            return '/logo-no-background.png'
+    computed: {
+        logoSrc() {
+            return '/logo-new.webp'
         }
     }
 }
 </script>
 <style scoped>
-.numberStyle{
+.numberStyle {
     color: #53D3B9;
     font-weight: 500;
     padding-right: 6px;
 }
-.textStyle{
-    color:#CCD6F6;
+
+.textStyle {
+    color: #CCD6F6;
     font-weight: 500;
     font-size: 14px;
 }
--- src/views/About.vue
+++ src/views/About.vue
@@ -6,48 +6,56 @@
                     <span class="numberStyle">01.</span> <span class="headerStyle text-md-h4 text-h4">About me</span>
                 </div>
                 <div class="horizontal w-25 mt-6 ml-2"></div>
-            </div>      
+            </div>
             <v-container class="px-0">
                 <v-row>
                     <v-col cols="12" md="4">
-                        <v-img
-                        src="/home-img.jpg"
-                        class="mt-md-4 mx-md-0 w-100"
-                        cover
-                        rounded
-                        >                      
+                        <v-img src="/home-img.jpg" class="mt-md-4 mx-md-0 w-100" cover rounded>
                         </v-img>
                     </v-col>
                     <v-col cols="12" md="8">
-                        <div class="textStyle mt-3">I am a seasoned software engineer with over a decade of experience in the industry, having embarked on my journey back in 2013. My technical expertise spans a diverse array of technologies and frameworks, reflecting my passion for continuous learning and adaptation in the ever-evolving field of software development.</div>
-                        <div class="textStyle mt-3">I have successfully delivered numerous projects across various domains, demonstrating a strong ability to solve complex problems and create robust, scalable applications. My journey has equipped me with a deep understanding of software design principles, agile methodologies, and the latest industry trends.</div>
-                        <div class="textStyle mt-3">I thrive in collaborative environments, leveraging my skills to drive innovation and efficiency. Whether working independently or as part of a team, my goal is to contribute to the development of high-quality software that meets user needs and exceeds expectations.</div>
+                        <div class="textStyle mt-3">I am a seasoned software engineer with over a decade of experience
+                            in the industry, having embarked on my journey back in 2013. My technical expertise spans a
+                            diverse array of technologies and frameworks, reflecting my passion for continuous learning
+                            and adaptation in the ever-evolving field of software development.</div>
+                        <div class="textStyle mt-3">I have successfully delivered numerous projects across various
+                            domains, demonstrating a strong ability to solve complex problems and create robust,
+                            scalable applications. My journey has equipped me with a deep understanding of software
+                            design principles, agile methodologies, and the latest industry trends.</div>
+                        <div class="textStyle mt-3">I thrive in collaborative environments, leveraging my skills to
+                            drive innovation and efficiency. Whether working independently or as part of a team, my goal
+                            is to contribute to the development of high-quality software that meets user needs and
+                            exceeds expectations.</div>
                         <div class="textStyle mt-3">Here are a few technologies I’ve been working with recently:</div>
                         <div class="mt-5">
                             <v-row>
                                 <v-col>
                                     <v-list>
-                                        <v-list-item v-for="language in languagesListOne" :key="language.name" class="px-0">
+                                        <v-list-item v-for="language in languagesListOne" :key="language.name"
+                                            class="px-0">
                                             <template v-slot:prepend>
                                                 <v-icon :icon="language.icon" size="x-small" color="#53D3B9"></v-icon>
                                             </template>
-                                            <v-list-item-title class="textStyleList mx-0 px-0">{{ language.name }}</v-list-item-title>
+                                            <v-list-item-title class="textStyleList mx-0 px-0">{{ language.name
+                                                }}</v-list-item-title>
                                         </v-list-item>
                                     </v-list>
                                 </v-col>
                                 <v-col>
                                     <v-list>
-                                        <v-list-item v-for="language in languagesListTwo" :key="language.name" class="px-0">
+                                        <v-list-item v-for="language in languagesListTwo" :key="language.name"
+                                            class="px-0">
                                             <template v-slot:prepend>
                                                 <v-icon :icon="language.icon" size="x-small" color="#53D3B9"></v-icon>
                                             </template>
-                                            <v-list-item-title class="textStyleList">{{ language.name }}</v-list-item-title>
+                                            <v-list-item-title class="textStyleList">{{ language.name
+                                                }}</v-list-item-title>
                                         </v-list-item>
                                     </v-list>
                                 </v-col>
                             </v-row>
                         </div>
-                    </v-col>                 
+                    </v-col>
                 </v-row>
             </v-container>
         </v-sheet>
@@ -56,21 +64,24 @@
 
 <script>
 export default {
-    data(){
-        return{
-            languagesListOne:[
-                {name:'Ethereum',icon:'mdi-arrow-right-top'},
-                {name:'Solidity',icon:'mdi-arrow-right-top'},
-                {name:'BlockChain',icon:'mdi-arrow-right-top'},
-                {name:'Node.js',icon:'mdi-arrow-right-top'},
-                {name:'Vue.Js',icon:'mdi-arrow-right-top'},
+    data() {
+        return {
+            languagesListOne: [
+                { name: 'Ethereum', icon: 'mdi-arrow-right-top' },
+                { name: 'Solidity', icon: 'mdi-arrow-right-top' },
+                { name: 'BlockChain', icon: 'mdi-arrow-right-top' },
+                { name: 'Node.js', icon: 'mdi-arrow-right-top' },
+                { name: 'Vue.Js', icon: 'mdi-arrow-right-top' },
+                { name: 'Vuetify', icon: 'mdi-arrow-right-top' },
+
             ],
-            languagesListTwo:[
-                {name:'Vuetify',icon:'mdi-arrow-right-top'},
-                {name:'Nuxt.Js',icon:'mdi-arrow-right-top'},
-                {name:'.NET Core',icon:'mdi-arrow-right-top'},
-                {name:'React',icon:'mdi-arrow-right-top'},
-                {name:'Tailwind Css',icon:'mdi-arrow-right-top'},
+            languagesListTwo: [
+                { name: 'Nuxt', icon: 'mdi-arrow-right-top' },
+                { name: '.NET Core', icon: 'mdi-arrow-right-top' },
+                { name: 'Laravel', icon: 'mdi-arrow-right-top' },
+                { name: 'Inertiajs', icon: 'mdi-arrow-right-top' },
+                { name: 'React', icon: 'mdi-arrow-right-top' },
+                { name: 'Tailwind Css', icon: 'mdi-arrow-right-top' },
             ]
         }
     }
@@ -78,28 +89,31 @@ export default {
 </script>
 
 <style scoped>
-.numberStyle{
+.numberStyle {
     color: #64FFDA;
     font-size: 25px;
     font-weight: 500;
     padding-right: 8px;
 }
-.headerStyle{
+
+.headerStyle {
     font-weight: 600;
-    color: #CCD6F6;  
+    color: #CCD6F6;
 }
-.horizontal{
+
+.horizontal {
     border-top: 1px solid #8892B0;
 }
-.textStyle{
-    font-weight: 400;
-    font-size: 17px;
-    color: #8892B0;
-}
-.textStyleList{
+
+.textStyle {
     font-weight: 400;
     font-size: 17px;
     color: #8892B0;
 }
 
+.textStyleList {
+    font-weight: 400;
+    font-size: 17px;
+    color: #8892B0;
+}
 </style>
